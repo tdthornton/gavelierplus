e705d3fbfeae035e567440724d03a92a0d44d1d8
Tom Thornton <tomthornton@toms-mbp.speedport_w_724v_09011603_06_006>
2020-11-13 00:02:58 +0100

    More updates to the new documents, including some finetuning of how fees are processed.

--- src/main/java/com/gavelier/gavelierplus/Endpoints.java
+++ src/main/java/com/gavelier/gavelierplus/Endpoints.java
@@ -247,122 +247,127 @@ public class Endpoints {
 
         LOGGER.info("auctkiuuijjon id " + currentAuctionId);
 
-            Auction currentAuction = dynamoDBService.getOneAuctionById(currentAuctionId, principal.getName());
+        Auction currentAuction = dynamoDBService.getOneAuctionById(currentAuctionId, principal.getName());
 
-            List<Integer> errors = new ArrayList<Integer>();
+        List<Integer> errors = new ArrayList<Integer>();
 
-            for (Lot lot : lotListWrapper.getLots()) {
+        for (Lot lot : lotListWrapper.getLots()) {
+            lot = applySellerFees(lot, currentAuction);
+            if (lot.getBuyerNumber() > 0) {
                 if (isValidSale(lot)) {
                     lot = applyBuyerFees(lot, currentAuction);
-                    lot = applySellerFees(lot, currentAuction);
                     dynamoDBService.updateLotSaleOnly(lot);
                 } else {
                     errors.add(lot.getLotNumber());
                 }
-            }
-
-            LOGGER.info(errors.toString());
-
-            if (errors.size() > 0) {
-                return "redirect:/auctioneering?auctionId=" + lotListWrapper.getLots().get(0).getAuctionId() + "&page="
-                        + page + "&error=" + URLEncoder.encode("Lots not saved: " + errors.toString(), "UTF-8");
             } else {
-                return "redirect:/auctioneering?auctionId=" + lotListWrapper.getLots().get(0).getAuctionId() + "&page="
-                        + page;
+                lot.setSalePrice(new BigDecimal("0.00").setScale(2));
+                dynamoDBService.updateLotSaleOnly(lot);
             }
-        
+        }
+
+        LOGGER.info(errors.toString());
+
+        if (errors.size() > 0) {
+            return "redirect:/auctioneering?auctionId=" + lotListWrapper.getLots().get(0).getAuctionId() + "&page="
+                    + page + "&error=" + URLEncoder.encode("Lots not saved: " + errors.toString(), "UTF-8");
+        } else {
+            return "redirect:/auctioneering?auctionId=" + lotListWrapper.getLots().get(0).getAuctionId() + "&page="
+                    + page;
+        }
 
     }
 
     private Lot applyBuyerFees(Lot lot, Auction auction) {
 
-            BigDecimal totalBuyerFees = new BigDecimal("0.00");
+        BigDecimal totalBuyerFees = new BigDecimal("0.00");
 
-            BigDecimal feeUnderTen = new BigDecimal("0.00");
+        BigDecimal feeUnderTen = new BigDecimal("0.00");
 
-            if (auction.getInputBuyerFeeMinimum() != null) {
-                feeUnderTen = auction.getInputBuyerFeeMinimum().setScale(2, RoundingMode.HALF_UP);
-            }
+        if (auction.getInputBuyerFeeMinimum() != null) {
+            feeUnderTen = auction.getInputBuyerFeeMinimum().setScale(2, RoundingMode.HALF_UP);
+        }
 
-            BigDecimal fixedAdditionalFee = new BigDecimal("0.00");
+        BigDecimal fixedAdditionalFee = new BigDecimal("0.00");
 
-            if (auction.getInputBuyerFeeFixed() != null) {
-                fixedAdditionalFee = auction.getInputBuyerFeeFixed().setScale(2, RoundingMode.HALF_UP);
+        if (auction.getInputBuyerFeeFixed() != null) {
+            fixedAdditionalFee = auction.getInputBuyerFeeFixed().setScale(2, RoundingMode.HALF_UP);
+        } else {
+            fixedAdditionalFee = new BigDecimal("0.00").setScale(2, RoundingMode.HALF_UP);
+        }
+
+        totalBuyerFees = totalBuyerFees.add(fixedAdditionalFee);
+
+        if (lot.getSalePrice() != null) {
+
+            BigDecimal percentageFee = lot.getSalePrice()
+                    .multiply(new BigDecimal("0." + auction.getInputBuyerFeePercentage()))
+                    .setScale(2, RoundingMode.HALF_UP);
+
+            if (lot.getSalePrice().compareTo(new BigDecimal("10.00")) > 0) {
+                totalBuyerFees = totalBuyerFees.add(percentageFee);
             } else {
-                fixedAdditionalFee = new BigDecimal("0.00").setScale(2, RoundingMode.HALF_UP);
+                totalBuyerFees = totalBuyerFees.add(feeUnderTen);
             }
 
-            totalBuyerFees = totalBuyerFees.add(fixedAdditionalFee);
+            lot.setCostToBuyer(lot.getSalePrice().add(totalBuyerFees).setScale(2, RoundingMode.HALF_UP));
+            lot.setSalePrice(lot.getSalePrice().setScale(2, RoundingMode.HALF_UP));
 
-            if (lot.getSalePrice() != null) {
+        }
 
-                BigDecimal percentageFee = lot.getSalePrice()
-                        .multiply(new BigDecimal("0." + auction.getInputBuyerFeePercentage()))
-                        .setScale(2, RoundingMode.HALF_UP);
-
-                if (lot.getSalePrice().compareTo(new BigDecimal("10.00")) > 0) {
-                    totalBuyerFees = totalBuyerFees.add(percentageFee);
-                } else {
-                    totalBuyerFees = totalBuyerFees.add(feeUnderTen);
-                }
-
-                lot.setCostToBuyer(lot.getSalePrice().add(totalBuyerFees).setScale(2, RoundingMode.HALF_UP));
-                lot.setSalePrice(lot.getSalePrice().setScale(2, RoundingMode.HALF_UP));
-
-            }
-
-            lot.setBuyerFees(totalBuyerFees.setScale(2, RoundingMode.HALF_UP));
-            
+        lot.setBuyerFees(totalBuyerFees.setScale(2, RoundingMode.HALF_UP));
 
         return lot;
     }
 
     private Lot applySellerFees(Lot lot, Auction auction) {
-        
 
-            BigDecimal finalPaymentToSeller;
+        BigDecimal finalPaymentToSeller;
 
-            
+        BigDecimal minimumFee = new BigDecimal("0.00");
+        BigDecimal totalFees = new BigDecimal("0.00");
 
-            BigDecimal minimumFee = new BigDecimal("0.00");
-            BigDecimal totalFees = new BigDecimal("0.00");
+        if (auction.getInputSellerFeeMinimum() != null) {
+            minimumFee = auction.getInputSellerFeeMinimum().setScale(2, RoundingMode.HALF_UP);
+        }
 
-            if (auction.getInputSellerFeeMinimum() != null) {
-                minimumFee = auction.getInputSellerFeeMinimum().setScale(2, RoundingMode.HALF_UP);
-            }
+        if (auction.getInputSellerFeeFixed() != null) {
+            totalFees = totalFees.add(auction.getInputSellerFeeFixed());
+        }
 
-            if (auction.getInputSellerFeeFixed() != null ) {
-                totalFees = totalFees.add(auction.getInputSellerFeeFixed());
-            }
+        if (lot.getSalePrice() != null) {
 
-            if (lot.getSalePrice() != null) {
+            finalPaymentToSeller = lot.getSalePrice().setScale(2, RoundingMode.HALF_UP);
 
-                finalPaymentToSeller = lot.getSalePrice().setScale(2, RoundingMode.HALF_UP);
+            BigDecimal percentageFee = lot.getSalePrice()
+                    .multiply(new BigDecimal("0." + auction.getInputSellerFeePercentage()))
+                    .setScale(2, RoundingMode.HALF_UP);
 
-                BigDecimal percentageFee = lot.getSalePrice()
-                        .multiply(new BigDecimal("0." + auction.getInputSellerFeePercentage())).setScale(2, RoundingMode.HALF_UP);
-
-                if (percentageFee.compareTo(minimumFee) > 0) {
-                    totalFees = totalFees.add(percentageFee);
-                } else {
-                    totalFees = totalFees.add(minimumFee);
-                }
-
+            if (percentageFee.compareTo(minimumFee) > 0) {
+                totalFees = totalFees.add(percentageFee);
             } else {
-
-                finalPaymentToSeller = new BigDecimal("0.00").setScale(2, RoundingMode.HALF_UP);
+                totalFees = totalFees.add(minimumFee);
             }
 
-            lot.setSellerFees(totalFees);
+        } else {
 
-            lot.setPaymentToSeller(finalPaymentToSeller.subtract(totalFees));
+            finalPaymentToSeller = new BigDecimal("0.00").setScale(2, RoundingMode.HALF_UP);
+        }
+
+        lot.setSellerFees(totalFees);
+
+        lot.setPaymentToSeller(finalPaymentToSeller.subtract(totalFees));
 
         return lot;
     }
 
     private boolean isValidSale(Lot lot) {
 
-        if (lot.getBuyerNumber() < 1 && (lot.getSalePrice() != null && lot.getSalePrice() != new BigDecimal("0.00"))) {
+        if (lot.getBuyerNumber() < 1 && (lot.getSalePrice() != null && lot.getSalePrice() != new BigDecimal("0.00")) && lot.getSalePrice() != new BigDecimal("0")) {
+            return false;
+        }
+
+        if (lot.getBuyerNumber() > 0 && (lot.getSalePrice() == null || lot.getSalePrice() == new BigDecimal("0.00")) || lot.getSalePrice() == new BigDecimal("0") || lot.getSalePrice().equals(new BigDecimal("0.00")) || lot.getSalePrice().equals(new BigDecimal("0"))) {
             return false;
         }
 
